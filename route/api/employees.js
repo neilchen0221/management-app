0255d3ff1ac9fa30312dc655291003454eb6e5fc
Neil Chen <sxch5319@gmail.com>
2018-12-29 16:15:37 +1100

    employee route

---
+++ route/api/employees.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+const Employee = require('../../models/Employee');
+
+// @route  GET api/employees
+// @desc   Get All Employees
+router.get('/', (req, res) => {
+  Employee.find()
+    .sort({ firstName: 1 })
+    .then(employees => res.json(employees));
+});
+
+// @route  POST api/employees
+// @desc   Create A Employee
+router.post('/', (req, res) => {
+  const newEmployee = new Employee({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    title: req.body.title
+  });
+  newEmployee.save().then(employee => res.json(employee));
+});
+
+// @route  PUT api/employees/:id
+// @desc   Update A Employee
+router.put('/:id', (req, res) => {
+  Employee.update({ _id: req.params.id }, req.body)
+    .then(employee => {
+      res.send(employee);
+    })
+    .catch(err => res.status(404).send('Employee not found.'));
+});
+
+// @route  DELETE api/employees/:id
+// @desc   Delete A Employee
+router.delete('/:id', (req, res) => {
+  Employee.findById(req.params.id)
+    .then(employee => employee.remove().then(() => res.send('Employee is successfully deleted.')))
+    .catch(err => res.status(404).send('Employee not found.'));
+});
+
+module.exports = router;
