e52b270bfbfa70b9b6128e5fddb7ee8220dda635
Neil Chen <sxch5319@gmail.com>
2019-01-02 21:37:26 +1100

    react router added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Route, HashRouter as Router, Switch, Redirect } from 'react-router-dom';
 import AppNavbar from './components/AppNavbar';
+import Dashboard from './components/Dashboard';
+import ProjectList from './components/ProjectList';
+import ProjectDetail from './components/ProjectDetail';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <AppNavbar />
-      </div>
+      <Router>
+        <AppNavbar>
+          <Switch>
+            <Redirect exact path="/" to="/dashboard" />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/projects" component={ProjectList} />
+            <Route exact path="/projects/:id" component={ProjectDetail} />
+          </Switch>
+        </AppNavbar>
+      </Router>
     );
   }
 }
